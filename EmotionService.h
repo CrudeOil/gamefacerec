c2bbdff5a1e6e851755098086f369682f428abce
AMDraisma <adraisma@gmail.com>
2017-02-10 16:09:14 +0100

    I am doing a thing

---
+++ EmotionService.h
@@ -0,0 +1,22 @@
+#include "cstdio"
+
+namespace LaNoirEmoRec {
+    class EmotionService {
+        int x;
+        public:
+            EmotionService() {
+                x = 0;
+            }
+
+            void setX(int newx) {
+                this->x = newx;
+            }
+
+            char* toString() {
+                char* buffer = new char[32];
+                sprintf(buffer, "%d", this->x);
+                return buffer;
+            }
+        
+    };
+}
---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+#include "d3d11.h"
+#include "EmotionService.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout<<"halp";
+    LaNoirEmoRec::EmotionService* ems = new LaNoirEmoRec::EmotionService();
+    
+    
+}
