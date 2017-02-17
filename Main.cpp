503d8a16e8ccaf546d2e36d11902e1650aa7ff60
CrudeOil <adraisma@gmail.com>
2017-02-17 09:50:06 +0100

    Reordered main function calls

--- Main.cpp
+++ Main.cpp
@@ -8,8 +8,9 @@ using namespace GameEmoRec;
 int main(int argc, char* argv[]) {
     cout<<"Starting Game emotion recognition"<<endl;
 
+    GameCapture* pGameCapture = new GameCapture("chrome.exe");
     EmotionService* pEmotionService = new EmotionService();
     pEmotionService->start();
-    cout<<pEmotionService->toString();
-    GameCapture* pGameCapture = new GameCapture("chrome.exe");
+    delete pGameCapture;
+    delete pEmotionService;
 }
