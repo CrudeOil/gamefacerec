ccaa2d309adb777d7ac954c0c9292cdaebde39b7
CrudeOil <adraisma@gmail.com>
2017-03-21 15:39:05 +0100

    Added dx include

--- build.bat
+++ build.bat
@@ -2,12 +2,13 @@
 
 set vcdir="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
 set affydir="C:\Program Files (x86)\Affectiva\AffdexSDK"
+set dx9dir="C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)"
 
 call %vcdir%\vcvarsall.bat x64
 
 cd build
 
-set compileflags=/Od /W4 /EHsc /I ../include /I %affydir%\include
+set compileflags=/Zi /W4 /EHsc /I ../include /I %affydir%\include /I %dx9dir%\Include
 set linkerflags=/Fe:gamefacerec.exe
 
 set files=../src/Affydex.cpp ../src/EmotionService.cpp ../src/GameCapture.cpp ../src/Main.cpp
